d431127c887d4486764aaf4b55dbb155b8125331
yuting <tim23656@gmail.com>
2019-11-23 23:16:13 +0800

    update strokrect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -78,12 +78,12 @@ export class AppComponent  implements OnInit{
       // 畫上框框
       ctx.strokeStyle = '#00FFFF';
       ctx.lineWidth = 2;
-      ctx.strokeRect(x, y, width, height);
+      ctx.strokeRect((x + x * 0.8), (y + y * 0.6), width, height);
       // 畫上背景
       ctx.fillStyle = '#00FFFF';
       const textWidth = ctx.measureText(prediction.class).width;
       const textHeight = parseInt(font, 10); // base 10
-      ctx.fillRect(x * 0.8, y * 0.6, textWidth + 4, textHeight + 4);
+      ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
     });
 
     // 畫上標籤
